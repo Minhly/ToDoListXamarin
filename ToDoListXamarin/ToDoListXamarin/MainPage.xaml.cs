9efee98cda23beb35630c852b0ec58fa6ba3cba3
Minh Yong Ly <minh.y.ly@hotmail.com>
2022-01-11 17:48:03 +0100

    Add project files.

---
+++ ToDoListXamarin/ToDoListXamarin/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ToDoListXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void HandleTextChanged(object sender, TextChangedEventArgs args)
+        {
+            Console.WriteLine(args.NewTextValue);
+        }
+
+        public void HandleEnterPress(object sender, EventArgs args)
+        {
+            Console.WriteLine("Enter Pressed");
+            Label newToDo = new Label();
+            newToDo.Text = InputField.Text;
+            newToDo.FontSize = 20;
+            ToDoList.Children.Add(newToDo);
+            InputField.Text = "";
+        }
+    }
+}
